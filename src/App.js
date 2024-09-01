9d282cd7305a8efa92f23b68aba883624878a7a1
NAVEEN15020189 <ramadevi15020@gmail.com>
2024-09-01 11:35:59 +0530

    2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Routing from './FullAdminDashBoard/Routing';
+function App() {
+  
+  return (
+   <div className="App" >
+
+ <Routing></Routing> 
+
+  
+
+    </div>
+  );
+}
+
+export default App;
